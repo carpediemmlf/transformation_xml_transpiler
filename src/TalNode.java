5dd9f9a15565e25ce2f17c1c77a3828e899e4621
trish25tennant <53783902+trish25tennant@users.noreply.github.com>
2019-08-08 16:54:55 +0100

    Update TalNode.java

--- src/TalNode.java
+++ src/TalNode.java
@@ -1,27 +1,192 @@
 public class TalNode {
     private String name;
     private String type;
+    private String xLoc;
+    private String yLoc;
+    private HashMap<String,String> simpleInfo = new HashMap<>();
 
-    public TalNode (){}
 
-    public void setName(String name) {
-        this.name = name;
+
+    protected TalNode (String name, String type){
+
+        this.name=name;
+
+        this.type=type;
+
+        simpleInfo.put("name",name);
+
+        simpleInfo.put("type",type);
+
     }
 
-    public void setType(String type) {
-        this.type = type;
-    }
 
-    public String getName(){
-        return name;
-    }
-    public String getType(){
-        return type;
-    }
 
     @Override
-    public String toString() {
-        return name;
-    }
-}
 
+    public String toString() {
+
+        return name;
+
+    }
+
+
+
+    // Getters
+
+    public String getName(){
+
+        return name;
+
+    }
+
+
+
+    public String getType(){
+
+        return type;
+
+    }
+
+
+
+    public String getxLoc() {
+
+        return xLoc;
+
+    }
+
+
+
+    public String getyLoc() {
+
+        return yLoc;
+
+    }
+
+
+
+    public HashMap<String, String> getSimpleInfo() {
+
+        return simpleInfo;
+
+    }
+
+
+
+    /* public String getDescription() {
+
+        return description;
+
+    }
+
+
+
+    public String getFilename() {
+
+        return filename;
+
+    }
+
+
+
+    public String getSeparator() {
+
+        return separator;
+
+    }
+
+
+
+    public String getEnclosure() {
+
+        return enclosure;
+
+    }
+
+
+
+    public ArrayList<String> getFields() {
+
+        return fields;
+
+    }*/
+
+
+
+    // Setters
+
+    public void setName(String name) {
+
+        this.name = name;
+
+    }
+
+
+
+    public void setType(String type) {
+
+        this.type = type;
+
+    }
+
+
+
+    public void setxLoc(String xLoc) {
+
+        this.xLoc = xLoc;
+
+    }
+
+
+
+    public void setyLoc(String yLoc) { this.yLoc = yLoc; }
+
+
+
+    public void setDescription(String description) {
+
+        simpleInfo.put("description",description);
+
+    }
+
+
+
+    public void setFilename(String filename) {
+
+        simpleInfo.put("filename",filename);
+
+    }
+
+
+
+    public void setSeparator(String separator) {
+
+        simpleInfo.put("separator",separator);
+
+    }
+
+
+
+    public void setEnclosure(String enclosure) {
+
+        simpleInfo.put("enclosure",enclosure);
+
+    }
+
+
+
+    public void addSimpleInfo (String infoName, String data){
+
+        simpleInfo.put(infoName,data);
+
+    }
+
+
+
+   /* public void setFields (ArrayList<String> fields){
+
+        simpleInfo.put("fields",fields);
+
+    }*/
+
+}
