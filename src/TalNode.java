bb8efec93514cad981defa41fb96fb578ccdf2b9
trish25tennant <53783902+trish25tennant@users.noreply.github.com>
2019-08-12 15:51:31 +0100

    Update TalNode.java

--- src/TalNode.java
+++ src/TalNode.java
@@ -1,182 +1,111 @@
-public class TalNode {
-    private String name;
-    private String type;
-    private String xLoc;
-    private String yLoc;
-    private HashMap<String,String> simpleInfo = new HashMap<>();
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.builder.GraphTypeBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
-    protected TalNode (String name, String type){
-        this.name=name;
-        this.type=type;
-        simpleInfo.put("name",name);
-        simpleInfo.put("type",type);
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.*;
+import java.util.*;
+
+public class TalToGraph {
+
+    private Document document;
+    Graph<TalNode, DefaultEdge> graph;
+
+    public TalToGraph(){
+
+        try {
+            // setting up documents to read from and write to, as xml document objects
+            InputStream inputStream = new FileInputStream(new File("talend_input_output.item")); //PROBLEM: when path is translated.xml, graph is NOT drawn
+
+            DocumentBuilderFactory Tfactory = DocumentBuilderFactory.newDefaultInstance();
+            DocumentBuilder Tbuilder = Tfactory.newDocumentBuilder();
+
+            document = Tbuilder.parse(inputStream);
+
+            graph=createGraph();
+            addVertices();
+            addEdges();
+            System.out.println(graph);
+
+        } catch (FileNotFoundException notFound){
+            System.out.println("File wasn't found");
+        } catch (IOException IoE){
+            System.out.println("IoE");
+        } catch (SAXException SAX){
+            System.out.println("SAX");
+        } catch (ParserConfigurationException parse){
+            System.out.println("Parse exception");
+        }
     }
 
-    @Override
-
-    public String toString() {
-
-        return name;
-
+    private Graph<TalNode, DefaultEdge> createGraph(){
+        return GraphTypeBuilder.<TalNode,DefaultEdge>directed().allowingMultipleEdges(true).allowingSelfLoops(false).edgeClass(DefaultEdge.class).weighted(false).buildGraph();
     }
 
+    public void addVertices(){
+        NodeList vertices = document.getElementsByTagName("node");
+        for (int i = 0; i < vertices.getLength(); i++){
+            // call method, passing through xml step, and takes in the data
+            String vertexName = vertices.item(i).getChildNodes().item(1).getAttributes().getNamedItem("value").getNodeValue();
+            String vertexType = vertices.item(i).getAttributes().getNamedItem("componentName").getNodeValue();
+            TalNode vertex = new TalNode();
+            vertex.setName(vertexName);
+            vertex.setType(vertexType);
 
-    // Getters
-
-    public String getName(){
-
-        return name;
-
+            graph.addVertex(vertex);
+        }
     }
 
+    public void addEdges() {
+        Map<String, List<String>> myMap2 = new HashMap<>();
+        List<String> link = new ArrayList<>();
+        NodeList edges = document.getElementsByTagName("connection");
+        for (int i = 0; i < edges.getLength(); i++) {
+            Node linkNode = edges.item(i);
+            if (linkNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element linkElement = (Element) linkNode;
+                String sourceName = linkElement.getAttribute("source");
+                String targetName = linkElement.getAttribute("target");
+                link.add(linkElement.getAttribute("connectorName"));
+                link.add(linkElement.getAttribute("source"));
+                link.add(linkElement.getAttribute("target"));
+                myMap2.put(linkElement.getAttribute("label"), link);
+                System.out.println(myMap2);
 
 
-    public String getType(){
+                TalNode source = null;
+                TalNode target = null;
 
-        return type;
+                Set vertexSet = graph.vertexSet();
+                Iterator vertexIterator = vertexSet.iterator();
 
-    }
-
-
-
-    public String getxLoc() {
-
-        return xLoc;
-
-    }
-
-
-
-    public String getyLoc() {
-
-        return yLoc;
-
-    }
-
-
-
-    public HashMap<String, String> getSimpleInfo() {
-
-        return simpleInfo;
-
-    }
-
-
-
-     public String getDescription() {
-
-        return description;
-
-    }
-
-
-
-    public String getFilename() {
-
-        return filename;
-
-    }
-
-
-
-    public String getSeparator() {
-
-        return separator;
-
-    }
-
-
-
-    public String getEnclosure() {
-
-        return enclosure;
-
-    }
-
-
-
-    public ArrayList<String> getFields() {
-
-        return fields;
-
-    }*/
-
-
-
-    // Setters
-
-    public void setName(String name) {
-
-        this.name = name;
-
+                while (vertexIterator.hasNext()) {
+                    TalNode vertex = (TalNode) vertexIterator.next();
+                    if (vertex.getName().equals(sourceName)) {
+                        source = vertex;
+                    }
+                    if (vertex.getName().equals(targetName)) {
+                        target = vertex;
+                    }
+                }
+                if (source != null & target != null) {
+                    graph.addEdge(source, target);
+                } else {
+                    System.out.println("Nodes not found in graph");
+                }
+            }
+        }
     }
 
 
-
-    public void setType(String type) {
-
-        this.type = type;
-
+    public Graph<TalNode, DefaultEdge> getGraph(){
+        return graph;
     }
-
-
-
-    public void setxLoc(String xLoc) {
-
-        this.xLoc = xLoc;
-
-    }
-
-
-
-    public void setyLoc(String yLoc) { this.yLoc = yLoc; }
-
-
-
-    public void setDescription(String description) {
-
-        simpleInfo.put("description",description);
-
-    }
-
-
-
-    public void setFilename(String filename) {
-
-        simpleInfo.put("filename",filename);
-
-    }
-
-
-
-    public void setSeparator(String separator) {
-
-        simpleInfo.put("separator",separator);
-
-    }
-
-
-
-    public void setEnclosure(String enclosure) {
-
-        simpleInfo.put("enclosure",enclosure);
-
-    }
-
-
-
-    public void addSimpleInfo (String infoName, String data){
-
-        simpleInfo.put(infoName,data);
-
-    }
-
-
-
-   /* public void setFields (ArrayList<String> fields){
-
-        simpleInfo.put("fields",fields);
-
-    }*/
-
 }
