fdaab8812c8b8494d59a2858f7e618e3ebec1d19
trish25tennant <53783902+trish25tennant@users.noreply.github.com>
2019-08-12 16:41:39 +0100

    Update ReadXMLFile.java

--- src/ReadXMLFile.java
+++ src/ReadXMLFile.java
@@ -15,7 +15,7 @@ import java.util.stream.Stream;
 
 public class ReadXMLFile {
 
-    private HashMap <String, String> myHashMap;
+    private HashMap<String, String> myHashMap;
 
 
     public HashMap getVertices(String fileName) throws ParserConfigurationException, IOException, SAXException {
@@ -42,32 +42,28 @@ public class ReadXMLFile {
         doc.getDocumentElement().normalize();
 
         NodeList nList = doc.getElementsByTagName("node");
-        // System.out.println("----------------------------");
         for (int temp = 0; temp < nList.getLength(); temp++) {
             Node nNode = nList.item(temp);
-            HashMap map=new LinkedHashMap();
+            HashMap map = new LinkedHashMap();
             if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                 Element eElement = (Element) nNode;
 
                 System.out.println("Node : " + eElement.getAttribute("componentName"));
 
                 for (int a = 1; a < nNode.getChildNodes().getLength(); a++) {
-                   // HashMap<String, String> hashMap=new LinkedHashMap<>();
                     if ((nNode.getChildNodes().item(a).getNodeType() == 1)) {
                         Node childNode = nNode.getChildNodes().item(a);
-
 
 
                         String key = nNode.getChildNodes().item(a).getAttributes().item(1).toString().replaceAll("name=", "");
                         Node value = nNode.getChildNodes().item(a).getAttributes().getNamedItem("value");
                         if (key != null && value != null) {
-                           map.put(key, value.toString().replaceAll("value=", ""));
-                            // System.out.println(value.toString().replaceAll("value=", ""));
+                            map.put(key, value.toString().replaceAll("value=", ""));
 
 
                         }
-                        if(childNode.hasChildNodes()&& Node.ELEMENT_NODE==nNode.getNodeType()) {
-                            HashMap childHashMap =new LinkedHashMap();
+                        if (childNode.hasChildNodes() && Node.ELEMENT_NODE == nNode.getNodeType()) {
+                            HashMap childHashMap = new LinkedHashMap();
                             for (int b = 0; b < childNode.getChildNodes().getLength(); b++) {
 
 
@@ -87,63 +83,17 @@ public class ReadXMLFile {
 
                     }
 
-                    }System.out.println(map);
-
                 }
-            //System.out.println(hashMap);
-
+                System.out.println(map);
 
             }
 
+
+        }
 
 
         return myHashMap;
 
-    }
-
-    public  String  getKey(String key) {
-        return (String) myHashMap.get(key);
-        /*
-        for (Object strKey : myHashMap.keySet()) {
-            if (strKey.equals("PRINT_HEADER")) {
-                System.out.println("Key : " + myHashMap.get(strKey));
-            } else {
-                System.out.println("no key specified");
-            }
-        }
-         */
-    }
-
-
-
-    public static Map getEdges(String fileName) throws ParserConfigurationException, IOException, SAXException {
-        Map<String, List<String>> myMap2 = new HashMap<>();
-        List<String> link = new ArrayList<>();
-        // Read in file located at same level as the root of the project folder.
-        File fXmlFile = new File(fileName);
-
-        // Build DOM Document. Throws ParserConfigurationException.
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-
-        // Throws IOException and SAXException.
-        Document doc = dBuilder.parse(fXmlFile);
-
-        //read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
-        doc.getDocumentElement().normalize();
-        NodeList linkList = doc.getElementsByTagName("connection");
-        for (int a = 0; a < linkList.getLength(); a++) {
-            Node linkNode = linkList.item(a);
-            if (linkNode.getNodeType() == Node.ELEMENT_NODE) {
-                Element linkElement = (Element) linkNode;
-                System.out.println("Connection: " + linkElement.getAttribute("label"));
-                link.add(linkElement.getAttribute("connectorName"));
-                link.add(linkElement.getAttribute("source"));
-                link.add(linkElement.getAttribute("target"));
-                myMap2.put(linkElement.getAttribute("label"), link);
-            }
-        }
-        return myMap2;
     }
 
 }
